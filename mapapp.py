10b03a51dbb9e6be550afae77cda11c4d401b673
EdyMol <36119589+chartist81@users.noreply.github.com>
2022-05-17 15:18:15 -0500

    Adding comments and correct marker color

--- mapapp.py
+++ mapapp.py
@@ -7,7 +7,7 @@ mapa = folium.Map(location=[-2.307362, -78.120463], zoom_start=10, tiles="Stamen
 fg = folium.FeatureGroup(name='My Map')
 
 # Adding marker to variable created above
-fg.add_child(folium.Marker(location=[-2.307362, -78.120463], popup="Marker", icon=folium.Icon(color='yellow')))
+fg.add_child(folium.Marker(location=[-2.307362, -78.120463], popup="Marker", icon=folium.Icon(color='lightgray')))
 
 # Passing fg variable changes to mapa variable.
 mapa.add_child(fg)
