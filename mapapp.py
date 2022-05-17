6bc4bea60ee3be0895cb1a52d0ac0807057ebb8c
EdyMol <36119589+chartist81@users.noreply.github.com>
2022-05-17 15:12:40 -0500

    Updated to add markers on map

--- mapapp.py
+++ mapapp.py
@@ -1,5 +1,8 @@
 import folium
 
-mapa = folium.Map(location=[-2.307362, -78.120463], zoom_start=15)
+mapa = folium.Map(location=[-2.307362, -78.120463], zoom_start=10, tiles="Stamen Terrain")
+fg = folium.FeatureGroup(name='My Map')
+fg.add_child(folium.Marker(location=[-2.307362, -78.120463], popup="Marker", icon=folium.Icon(color='yellow')))
+mapa.add_child(fg)
 mapa.save("Map1.html")
 print(type(mapa))
