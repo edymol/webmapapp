fb61f06d9efdb098ce17cab3464a0b707d0b0ae4
EdyMol <36119589+chartist81@users.noreply.github.com>
2022-05-18 05:23:02 -0500

    Using coords file to create a path in map

--- mapapp.py
+++ mapapp.py
@@ -5,6 +5,8 @@ import pandas
 data = pandas.read_csv("CoordData.csv")
 lat = list(data["lat"])
 lon = list(data["lon"])
+# to add elevation to map, create an elevation variable
+# el = list(data.elevation)
 
 # this creates a map location based on coordinates provided, places zoom % and what type of map "Bright Box" or current
 mapa = folium.Map(location=[-2.307362, -78.120463], zoom_start=8, tiles="Stamen Terrain")
@@ -13,9 +15,9 @@ mapa = folium.Map(location=[-2.307362, -78.120463], zoom_start=8, tiles="Stamen 
 fg = folium.FeatureGroup(name='My Map')
 
 # Adding marker to variable created above
-for lt, ln in zip(lat, lon):
+for lt, ln in zip(lat, lon): # add el to this iteration
     fg.add_child(folium.Marker(location=[lt, ln], popup=['Marker'], icon=folium.Icon(color='red')))
-
+    # add el to popup = str(el) + " m"
 
 # Passing fg variable changes to mapa variable.
 mapa.add_child(fg)
